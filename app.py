639a98dcc217ca31411a3d4998c9cbb48aa1c858
karthik <karthk.marappan@cloudzenix.com>
2024-03-04 02:01:36 -0600

    22

--- app.py
+++ app.py
@@ -280,10 +280,12 @@ def position_details_page():
 
     total_realized_pnl = sum(realized_pnl.values())
     total_unrealized_pnl = sum(unrealized_pnl.values())
+    total_pnl = total_realized_pnl + total_unrealized_pnl
+
 
     return render_template('position_details.html', realized_pnl=realized_pnl, unrealized_pnl=unrealized_pnl,
                            available_quantity=available_quantity, total_realized_pnl=total_realized_pnl,
-                           total_unrealized_pnl=total_unrealized_pnl, total_pnl=total_realized_pnl-total_realized_pnl, last_traded_prices=last_traded_prices)
+                           total_unrealized_pnl=total_unrealized_pnl, total_pnl=total_pnl, last_traded_prices=last_traded_prices)
 
 
 @app.route('/dashboard')
