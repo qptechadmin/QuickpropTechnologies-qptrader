91467e0a1dd08ec7f598d1cb5d88b178747de80f
karthik <karthk.marappan@cloudzenix.com>
2024-03-03 23:57:06 -0600

    sss

--- app.py
+++ app.py
@@ -281,6 +281,7 @@ def position_details_page():
         pnl_m2m[stock]['pnl'] += pnl
         pnl_m2m[stock]['m2m'] += m2m
         pnl_m2m[stock]['avg_price'] = avg_price
+        pnl_m2m[stock]['last_traded_price'] = last_traded_prices.get(stock, 0)
 
     return render_template('position_details.html', pnl_m2m=pnl_m2m, available_quantity=available_quantity)
 
