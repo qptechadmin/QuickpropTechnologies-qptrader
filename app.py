3605667592b37518ab856b333e8798a68fd6212a
karthik <karthk.marappan@cloudzenix.com>
2024-03-05 22:00:06 -0600

    ss

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ def position_details_page():
     instrument_token = 'NSE:INFY'  # Example instrument token for Infosys
     quote = kite.quote(instrument_token)
     for stock_symbol in last_traded_prices:
-        if last_traded_prices[stock_symbol] == Decimal('0.00')
+        if last_traded_prices[stock_symbol] == Decimal('0.00'):
             #last_traded_prices[stock_symbol] = Decimal(quote)
             last_traded_prices[stock_symbol] = Decimal(get_last_traded_price("SUNPHARMA"))
     unrealized_pnl = {}
